a5bd1057cb8e69b7adfb264367ae4851b07b5ba6
Ramon Onis <ramon.onis@nedap.com>
2024-01-23 16:41:43 +0100

    .

--- indoor.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in csv/indoor/*.csv; do
-  name=$(basename "$f" .csv)
-
-  gnuplot -c lol.gnuplot "${f}" "graphs/indoor/${name}.svg";
-done
--- outdoor.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in csv/outdoor/*.csv; do
-  name=$(basename "$f" .csv)
-
-  gnuplot -c lol.gnuplot "${f}" "graphs/outdoor/${name}.svg";
-done
